43feafce96f0b78829e02e8e373733487f87b9c8
koushiro <koushiro.cqx@gmail.com>
2025-02-14 01:50:45 +0800

    update doc

--- src/metrics/raw/mod.rs
+++ src/metrics/raw/mod.rs
@@ -1,7 +1,7 @@
-//! Raw metric types and implementations.
+//! Raw metric types and traits.
 //!
-//! This module contains the low-level implementations of metric types like numbers and atomic
-//! values, which serve as the foundation for higher-level metric abstractions.
+//! This module contains the low-level implementations of metric types, which serve as the
+//! foundation for higher-level metric abstractions.
 
 mod atomic;
 pub(crate) mod bucket;
