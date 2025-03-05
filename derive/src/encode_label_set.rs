d25171e06a4e69e801614d09a87a5fb781c7d5e1
koushiro <koushiro.cqx@gmail.com>
2025-03-05 22:39:19 +0800

    improve

--- derive/src/encode_label_set.rs
+++ derive/src/encode_label_set.rs
@@ -3,38 +3,52 @@ use quote::quote;
 
 pub fn expand_derive_encode_label_set(input: syn::DeriveInput) -> syn::Result<TokenStream> {
     let name = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-    let body: TokenStream = match input.data {
-        syn::Data::Struct(s) => match s.fields {
-            syn::Fields::Named(syn::FieldsNamed { named, .. }) => named
-                .into_iter()
-                .map(|f| {
-                    let ident = f.ident.unwrap();
-                    let ident_string = ident.to_string();
-                    quote! {
-                        (&#ident_string, &self.#ident).encode(encoder.label_encoder().as_mut())?;
-                    }
-                })
-                .collect(),
-            syn::Fields::Unnamed(_) => panic!("Can't derive `EncodeLabelSet` for tuple struct."),
-            syn::Fields::Unit => panic!("Can't derive `EncodeLabelSet` for unit struct."),
+    // Ensure we're deriving for a struct with named fields
+    let fields = match &input.data {
+        syn::Data::Struct(data) => match &data.fields {
+            syn::Fields::Named(syn::FieldsNamed { named, .. }) => named,
+            _ => {
+                return Err(syn::Error::new_spanned(
+                    input,
+                    "`EncodeLabelSet` can only be derived for structs with named fields.",
+                ))
+            },
         },
-        syn::Data::Enum(_) => panic!("Can't derive `EncodeLabelSet` for enum."),
-        syn::Data::Union(_) => panic!("Can't derive `EncodeLabelSet` for union."),
+        _ => {
+            return Err(syn::Error::new_spanned(
+                &input,
+                "`EncodeLabelSet` can only be derived for structs.",
+            ))
+        },
     };
 
-    let impl_block = quote! {
-        #[automatically_derived]
-        impl openmetrics_client::encoder::EncodeLabelSet for #name {
-            fn encode(&self, encoder: &mut dyn openmetrics_client::encoder::LabelSetEncoder) -> std::fmt::Result {
-                use openmetrics_client::encoder::EncodeLabel;
+    // Process all fields
+    let field_list = fields
+        .iter()
+        .map(|f| {
+            let ident = f.ident.as_ref().unwrap();
+            let ident_str = ident.to_string();
+            quote! {
+                (&#ident_str, &self.#ident).encode(encoder.label_encoder().as_mut())?
+            }
+        })
+        .collect::<Vec<_>>();
 
-                #body
+    // Generate the trait implementation
+    let expanded = quote! {
+        #[automatically_derived]
+        impl #impl_generics ::openmetrics_client::encoder::EncodeLabelSet for #name #ty_generics #where_clause {
+            fn encode(&self, encoder: &mut dyn ::openmetrics_client::encoder::LabelSetEncoder) -> ::std::fmt::Result {
+                use ::openmetrics_client::encoder::EncodeLabel;
+
+                #(#field_list;)*
 
                 Ok(())
             }
         }
     };
 
-    Ok(impl_block)
+    Ok(expanded)
 }
--- derive/src/encode_label_value.rs
+++ derive/src/encode_label_value.rs
@@ -2,41 +2,67 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 pub fn expand_derive_encode_label_value(input: syn::DeriveInput) -> syn::Result<TokenStream> {
-    let name = input.ident;
+    let name = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-    let body = match input.data {
-        syn::Data::Struct(_) => panic!("Can't derive `EncodeLabelValue` for struct."),
-        syn::Data::Enum(syn::DataEnum { variants, .. }) => {
-            let match_arms: TokenStream = variants
-                .into_iter()
-                .map(|v| {
-                    let variant = v.ident;
-                    quote! {
-                        #name::#variant => encoder.encode_str_value(stringify!(#variant))?,
-                    }
-                })
-                .collect();
-
-            quote! {
-                match self {
-                    #match_arms
-                }
-            }
+    // Ensure we're deriving for an enum
+    let data_enum = match &input.data {
+        syn::Data::Enum(data) => data,
+        _ => {
+            return Err(syn::Error::new_spanned(
+                &input,
+                "`EncodeLabelValue` can only be derived for enums",
+            ))
         },
-        syn::Data::Union(_) => panic!("Can't derive `EncodeLabelValue` for union."),
     };
 
-    let impl_block = quote! {
+    // Ensure the enum has at least one variant
+    if data_enum.variants.is_empty() {
+        return Err(syn::Error::new_spanned(
+            &input,
+            "`EncodeLabelValue` requires at least one variant in the enum",
+        ));
+    }
+
+    // Generate match arms for each variant
+    let variant_arms = data_enum
+        .variants
+        .iter()
+        .map(|variant| {
+            let variant_name = &variant.ident;
+
+            // Check that this is a unit variant (no fields)
+            match &variant.fields {
+                syn::Fields::Unit => {},
+                _ => {
+                    return Err(syn::Error::new_spanned(
+                        variant,
+                        "`EncodeLabelValue` can only be derived for enums with unit variants",
+                    ))
+                },
+            }
+
+            // The string representation is the variant name
+            let variant_str = variant_name.to_string();
+
+            Ok(quote! {
+                #name::#variant_name => encoder.encode_str_value(&#variant_str)?
+            })
+        })
+        .collect::<syn::Result<Vec<_>>>()?;
+
+    // Generate the trait implementation
+    let expanded = quote! {
         #[automatically_derived]
-        impl openmetrics_client::encoder::EncodeLabelValue for #name {
-            fn encode(&self, encoder: &mut dyn openmetrics_client::encoder::LabelEncoder) -> std::fmt::Result {
-
-                #body
-
+        impl #impl_generics ::openmetrics_client::encoder::EncodeLabelValue for #name #ty_generics #where_clause {
+            fn encode(&self, encoder: &mut dyn ::openmetrics_client::encoder::LabelEncoder) -> ::std::fmt::Result {
+                match self {
+                    #(#variant_arms,)*
+                }
                 Ok(())
             }
         }
     };
 
-    Ok(impl_block)
+    Ok(expanded)
 }
