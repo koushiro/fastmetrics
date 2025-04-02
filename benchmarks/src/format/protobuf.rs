cf6c3ac356f50311eea82c8e3bb7c7c27be0ea41
Qinxuan Chen <koushiro.cqx@gmail.com>
2025-04-02 17:59:03 +0800

    chore: add benchmarks about prometheus crate (#37)
    * chore: add benchmarks about prometheus crate
    
    * make clippy happy

--- benchmarks/src/format/protobuf.rs
+++ benchmarks/src/format/protobuf.rs
@@ -3,16 +3,36 @@ mod common;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use pprof::criterion::{Output, PProfProfiler};
 
-use crate::common::{setup_openmetrics_client_registry, setup_prometheus_client_registry};
+use crate::common::{
+    setup_openmetrics_client_registry, setup_prometheus_client_registry, setup_prometheus_registry,
+};
 
 fn bench_protobuf_encoding(c: &mut Criterion) {
     let mut group = c.benchmark_group("protobuf::encode");
 
     let metric_counts = [10, 100];
-    let observe_times = [10, 100, 1_000, 10_000, 100_000];
+    let observe_times = [100, 1_000, 10_000, 100_000];
 
     for count in metric_counts {
         for times in observe_times {
+            let id = format!("prometheus: {count} metrics * {times} observe times");
+            group.bench_function(id, |b| {
+                let registry = setup_prometheus_registry(count, times);
+
+                let mut buffer = Vec::new();
+
+                b.iter(|| {
+                    let metric_families = registry.gather();
+                    prometheus::Encoder::encode(
+                        &prometheus::ProtobufEncoder::new(),
+                        &metric_families,
+                        &mut buffer,
+                    )
+                    .unwrap();
+                    black_box(&mut buffer);
+                });
+            });
+
             let id = format!("prometheus_client: {count} metrics * {times} observe times");
             group.bench_function(id, |b| {
                 let registry = setup_prometheus_client_registry(count, times);
