b03ff4743c75e2ed4a55d16476ae4110cdae1d21
koushiro <koushiro.cqx@gmail.com>
2025-04-06 15:33:32 +0800

    chore: add more examples

---
+++ examples/src/family.rs
@@ -0,0 +1,70 @@
+use anyhow::Result;
+use openmetrics_client::{
+    encoder::{EncodeLabelSet, EncodeLabelValue},
+    format::text,
+    metrics::{
+        counter::Counter,
+        family::{Family, Unit},
+        histogram::{exponential_buckets, Histogram},
+    },
+    registry::Registry,
+};
+use rand::{
+    distr::{Distribution, StandardUniform},
+    Rng,
+};
+
+#[derive(Clone, Eq, PartialEq, Hash, EncodeLabelSet)]
+struct Labels {
+    operation: Operation,
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, EncodeLabelValue)]
+enum Operation {
+    Read,
+    Write,
+    List,
+    Delete,
+}
+
+impl Distribution<Labels> for StandardUniform {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Labels {
+        let operation = match rng.random_range(0..=3) {
+            0 => Operation::Read,
+            1 => Operation::Write,
+            2 => Operation::List,
+            _ => Operation::Delete,
+        };
+        Labels { operation }
+    }
+}
+
+fn main() -> Result<()> {
+    let mut registry = Registry::builder().with_namespace("demo").build();
+
+    let operation = Family::<Labels, Counter>::default();
+    let operation_bytes =
+        Family::<Labels, Histogram>::new(|| Histogram::new(exponential_buckets(1.0, 2.0, 10)));
+
+    registry
+        .register("operation", "Total number of operation", operation.clone())?
+        .register_with_unit(
+            "operation",
+            "Operation size of bytes",
+            Unit::Bytes,
+            operation_bytes.clone(),
+        )?;
+
+    let mut rng = rand::rng();
+    for _ in 0..100 {
+        let labels = rng.random::<Labels>();
+        operation.with_or_new(&labels, |counter| counter.inc());
+        operation_bytes.with_or_new(&labels, |hist| hist.observe(rng.random_range(0f64..2048f64)));
+    }
+
+    let mut output = String::new();
+    text::encode(&mut output, &registry)?;
+    println!("{}", &output);
+
+    Ok(())
+}
---
+++ examples/src/metrics.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use openmetrics_client::{
+    format::text,
+    metrics::{
+        counter::Counter,
+        gauge::Gauge,
+        histogram::{exponential_buckets, Histogram},
+    },
+    registry::Registry,
+};
+use rand::Rng;
+
+fn main() -> Result<()> {
+    let mut registry = Registry::builder().with_namespace("demo").build();
+
+    let counter = <Counter>::default();
+    let gauge = <Gauge>::default();
+    let histogram = Histogram::new(exponential_buckets(1.0, 2.0, 10));
+
+    registry
+        .register("my_counter", "My counter help", counter.clone())?
+        .register("my_gauge", "My gauge help", gauge.clone())?
+        .register("my_histogram", "My histogram help", histogram.clone())?;
+
+    let mut rng = rand::rng();
+    for _ in 0..100 {
+        counter.inc();
+        gauge.set(rng.random_range(-100..100i64));
+        histogram.observe(rng.random_range(0f64..2048f64));
+    }
+
+    let mut output = String::new();
+    text::encode(&mut output, &registry)?;
+    println!("{}", &output);
+
+    Ok(())
+}
