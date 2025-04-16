f9c32167ea0c381ee86658026f78d13368b5d61c
Qinxuan Chen <koushiro.cqx@gmail.com>
2025-04-16 17:18:27 +0800

    chore(docs): fix optional features (#52)
    * chore(docs): fix optional features
    
    * fix typo
    
    * make clippy happy

--- fastmetrics/src/format/protobuf/mod.rs
+++ fastmetrics/src/format/protobuf/mod.rs
@@ -15,7 +15,8 @@ use crate::{
 ///
 /// [OpenMetrics protobuf schema]: https://github.com/prometheus/OpenMetrics/blob/main/proto/openmetrics_data_model.proto
 #[allow(missing_docs)]
-pub mod openmetrics_data_model {
+#[allow(clippy::all)]
+mod openmetrics_data_model {
     include!(concat!(env!("OUT_DIR"), "/openmetrics.rs"));
 }
 
