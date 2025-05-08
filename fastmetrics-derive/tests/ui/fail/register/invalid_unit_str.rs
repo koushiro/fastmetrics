80e0d117852bbf355ecce95523076e841c267265
Qinxuan Chen <koushiro.cqx@gmail.com>
2025-05-08 16:06:37 +0800

    chore(derive): add some ui tests for register derive macro (#62)

---
+++ fastmetrics-derive/tests/ui/fail/register/invalid_unit_str.rs
@@ -0,0 +1,11 @@
+use fastmetrics::metrics::histogram::Histogram;
+use fastmetrics_derive::Register;
+
+#[derive(Default, Register)]
+struct Metrics {
+    /// Duration of HTTP requests
+    #[register(rename = "http_requests_duration", unit = Seconds)]
+    request_duration: Histogram,
+}
+
+fn main() {}
--- fastmetrics-derive/tests/ui_tests.rs
+++ fastmetrics-derive/tests/ui_tests.rs
@@ -2,7 +2,7 @@
 fn ui() {
     let t = trybuild::TestCases::new();
     // Tests that should compile successfully
-    t.pass("tests/ui/pass/*.rs");
+    t.pass("tests/ui/pass/**/*.rs");
     // Tests that should fail to compile with specific errors
-    t.compile_fail("tests/ui/fail/*.rs");
+    t.compile_fail("tests/ui/fail/**/*.rs");
 }
